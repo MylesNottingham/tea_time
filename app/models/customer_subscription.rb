b39fa912eaaf58c5681ba30f9c11eb22bf858449
Myles Nottingham <MylesNottingham@gmail.com>
2023-09-15 14:02:54 -0600

    Feat: Setup enums and defaults for CustomerSubscriptions

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -1,4 +1,16 @@
 class CustomerSubscription < ApplicationRecord
   belongs_to :customer
   belongs_to :subscription
+
+  enum status: [:active, :cancelled]
+  enum frequency: [:monthly, :quarterly, :semiannually, :annually]
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.status ||= 0
+    self.frequency ||= 0
+  end
 end

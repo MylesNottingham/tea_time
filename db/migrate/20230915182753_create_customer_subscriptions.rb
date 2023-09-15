f0736320a316929c176da8f7d370e3ef32f4ee6a
Myles Nottingham <MylesNottingham@gmail.com>
2023-09-15 13:23:10 -0600

    Fix: Add missing columns to CustomerSubscriptions

--- db/migrate/20230915182753_create_customer_subscriptions.rb
+++ db/migrate/20230915182753_create_customer_subscriptions.rb
@@ -3,6 +3,8 @@ class CreateCustomerSubscriptions < ActiveRecord::Migration[7.0]
     create_table :customer_subscriptions do |t|
       t.references :customer, null: false, foreign_key: true
       t.references :subscription, null: false, foreign_key: true
+      t.integer :status
+      t.integer :frequency
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_182827) do
   create_table "customer_subscriptions", force: :cascade do |t|
     t.bigint "customer_id", null: false
     t.bigint "subscription_id", null: false
+    t.integer "status"
+    t.integer "frequency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_customer_subscriptions_on_customer_id"

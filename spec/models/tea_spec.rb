03e4cc4eb9711de24921dbd9ee9bd208ca022d78
Myles Nottingham <MylesNottingham@gmail.com>
2023-09-16 21:31:40 -0600

    Test: Create model tests

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:subscription_teas) }
+    it { should have_many(:subscriptions).through(:subscription_teas) }
+    it { should have_many(:customer_subscriptions).through(:subscriptions) }
+    it { should have_many(:customers).through(:customer_subscriptions) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :temperature }
+    it { should validate_presence_of :brew_time }
+  end
 end
